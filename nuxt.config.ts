965d6b69b5f8f30e7ee1be53e64871ffa1511925
Yvan Brito <yvanbrito96@gmail.com>
2025-08-05 14:26:29 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,37 @@
+import Aura from '@primeuix/themes/aura'
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com',
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
+        },
+      ],
+    },
+  },
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  css: ['@/assets/main.css'],
+  modules: ['@primevue/nuxt-module'],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: '.p-dark',
+        },
+      },
+    },
+  },
+})
